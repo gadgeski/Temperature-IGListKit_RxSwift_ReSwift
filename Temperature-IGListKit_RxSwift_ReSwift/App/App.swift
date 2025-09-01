740b075315e2a50a2cbb9eace68636b4d71c4e9a
gadgeski <xtwh.ce5bil@gmail.com>
2025-09-01 23:20:20 +0900

    feat: (App/App.swift)

---
+++ Temperature-IGListKit_RxSwift_ReSwift/App/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  Tenperature-IGListKit_RxSwift_ReSwift
+//
+//  Created by Dev Tech on 2025/08/29.
+//
+
+import SwiftUI
+
+@main
+struct JapanTemperatureIGLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
