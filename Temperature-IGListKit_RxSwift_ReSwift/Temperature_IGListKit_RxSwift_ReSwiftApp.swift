57285e4b8ff4f7c7794b66453a5deb71d598309e
gadgeski <xtwh.ce5bil@gmail.com>
2025-09-01 23:18:51 +0900

    feat: (Temperature-IGListKit_RxSwift_ReSwiftApp.swift)

---
+++ Temperature-IGListKit_RxSwift_ReSwift/Temperature_IGListKit_RxSwift_ReSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tenperature_IGListKit_RxSwift_ReSwiftApp.swift
+//  Tenperature-IGListKit_RxSwift_ReSwift
+//
+//  Created by Dev Tech on 2025/08/29.
+//
+
+import SwiftUI
+
+
+struct Tenperature_IGListKit_RxSwift_ReSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
