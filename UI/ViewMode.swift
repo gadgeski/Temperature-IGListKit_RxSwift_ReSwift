7bd9f877fd26b177f6385370b0128ba6514e09c6
gadgeski <xtwh.ce5bil@gmail.com>
2025-09-06 17:23:53 +0900

    feat: (UI/ViewMode.swift)

---
+++ UI/ViewMode.swift
@@ -0,0 +1,13 @@
+//
+//  ViewMode.swift
+//  Temperature_IGListKit_RxSwift_ReSwift.entitlements
+//
+//  Created by Dev Tech on 2025/09/05.
+//
+
+import Foundation
+
+// NOTE: 画面モードはUI層の状態なのでUI配下に分離
+public enum ViewMode {
+    case dashboard, hourly, alerts, settings
+}
