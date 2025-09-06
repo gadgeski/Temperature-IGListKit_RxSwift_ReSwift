cd7affb574ec3cb4f6665e6c2ca2fccaedf3c97f
gadgeski <xtwh.ce5bil@gmail.com>
2025-09-06 17:22:37 +0900

    feat: (Theme/Color+Weather.swift)

---
+++ Theme/Color+Weather.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Weather.swift
+//  Temperature_IGListKit_RxSwift_ReSwift.entitlements
+//
+//  Created by Dev Tech on 2025/09/05.
+//
+
+import SwiftUI
+
+// 🎨 天気に合わせた動的カラー（UIテーマ）
+public extension Color {
+    static let sunnyGradientTop    = Color(red: 1.0, green: 0.8, blue: 0.4)
+    static let sunnyGradientBottom = Color(red: 1.0, green: 0.6, blue: 0.2)
+    static let rainyGradientTop    = Color(red: 0.4, green: 0.5, blue: 0.7)
+    static let rainyGradientBottom = Color(red: 0.2, green: 0.3, blue: 0.5)
+    static let cloudyGradientTop   = Color(red: 0.7, green: 0.7, blue: 0.8)
+    static let cloudyGradientBottom = Color(red: 0.5, green: 0.5, blue: 0.6)
+}
